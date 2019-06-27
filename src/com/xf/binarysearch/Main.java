ce74894a9ed62a6c7b8de6f17569b2683cf7a107
xfwangmm <xfwangmm@gmail.com>
2019-06-27 22:52:33 +0800

    ....

---
+++ src/com/xf/binarysearch/Main.java
@@ -0,0 +1,16 @@
+package com.xf.binarysearch;
+
+/**
+ * @author xfwaaang
+ * @create 2019-06-18 16:07
+ */
+public class Main {
+
+    public static void main(String[] args){
+        Solution solution = new Solution();
+
+        int[] nums = {-1,0,3,5,9,12};
+        int target = 3;
+        System.out.println(solution.search(nums, target));
+    }
+}
